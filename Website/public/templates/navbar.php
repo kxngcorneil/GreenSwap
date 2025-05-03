4273f4ad3da836292d338502a707bd51e0b5efcb
King Kiaku <b00160936@mytudublin.ie>
2025-05-03 13:49:41 +0100

    css fix (i think)

--- Website/public/templates/navbar.php
+++ Website/public/templates/navbar.php
@@ -1,69 +1,69 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html lang="en">
+    <?php session_start(); ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navbar</title>
-    <link rel="stylesheet" href="css/navbar.css">
-    <script src='../private/functions.js'></script>
-    <script src="https://kit.fontawesome.com/85df64fd4d.js" crossorigin="anonymous"></script>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Navbar</title>
+        <link rel="stylesheet" href="css/navbar.css">
+        <script src='../private/functions.js'></script>
+        <script src="https://kit.fontawesome.com/85df64fd4d.js" crossorigin="anonymous"></script>
+    </head>
 
-<body>
-    <nav class="navbar">
-        <div class="logo">
-            <img src="images/logo_whitebackground.png" alt="GreenSwap Logo">
-        </div>
-        <ul class="nav-links">
-            <li><a href="index.php" class="active"><i class="fas fa-home"></i> Home</a></li>
-            <li class="dropdown">
-                <a href="#"><i class="fas fa-list"></i> Market <i class="fa-solid fa-caret-down"></i></a>
-                <div class="Listings-Dropdown">
-                    <a href="Marketplace.php">All Products</a>
-                    <a href="productpages/Plants.php">Plants</a>
-                    <a href="productpages/Toys.php">Toys</a>
-                    <a href="productpages/Indoor_Furniture.php">Indoor Furniture</a>
-                    <a href="productpages/Outdoor_Furniture.php">Outdoor Furniture</a>
-                    <a href="productpages/Clothing.php">Clothing</a>
-                    <a href="productpages/Books.php">Books</a>
-                    <a href="productpages/Cars.php">Cars</a>
-                </div>
-            </li>
-            <li><a href="sustainability.php"><i class="fas fa-recycle"></i> Sustainability</a></li>
-            <li class="dropdown">
-                <a href="#"><i class="fas fa-list"></i> Listing <i class="fa-solid fa-caret-down"></i></a>
-                <div class="Listings-Dropdown">
-                    <a href='../../Website/private/CRUD/createProduct.php'>List New Product</a>
-                    <a href="#">Edit Existing Product</a>
-                    <a href="#">Delete Existing Product</a>
-                </div>
-            </li>
-        </ul>
-
-        <!-- Searchbar - Take all product names and allow the user to search -->
-        <div class="search-bar">
-            <input type="text" placeholder="Search items or categories">
-            <button><i class="fas fa-search"></i></button>
-        </div>
-
-        <?php
-        if (isset($_SESSION['Active']) && $_SESSION['Active'] === true) {
-            echo '<li class="dropdown">
-                    <p class="username">Welcome, ' . htmlspecialchars($_SESSION['Username']) . '! <i class="fa-solid fa-caret-down"></i></p>
+    <body>
+        <nav class="navbar">
+            <div class="logo">
+                <img src="images/logo_whitebackground.png" alt="GreenSwap Logo">
+            </div>
+            <ul class="nav-links">
+                <li><a href="index.php" class="active"><i class="fas fa-home"></i> Home</a></li>
+                <li class="dropdown">
+                    <a href="#"><i class="fas fa-list"></i> Market <i class="fa-solid fa-caret-down"></i></a>
                     <div class="Listings-Dropdown">
-                        <a href="Logout.php">Logout</a>
+                        <a href="Marketplace.php">All Products</a>
+                        <a href="productpages/Plants.php">Plants</a>
+                        <a href="productpages/Toys.php">Toys</a>
+                        <a href="productpages/Indoor_Furniture.php">Indoor Furniture</a>
+                        <a href="productpages/Outdoor_Furniture.php">Outdoor Furniture</a>
+                        <a href="productpages/Clothing.php">Clothing</a>
+                        <a href="productpages/Books.php">Books</a>
+                        <a href="productpages/Cars.php">Cars</a>
                     </div>
-                </li>';
-        } else {
-            echo '<div class="nav-buttons">
-                    <button class="join"><a href="signup.html">Get Started</a></button>
-                    <button class="login"><a href="login.php">Login</a></button>
-                 </div>';
-        }
-        ?>
-    </nav>
-</body>
+                </li>
+                <li><a href="sustainability.php"><i class="fas fa-recycle"></i> Sustainability</a></li>
+                <li class="dropdown">
+                    <a href="#"><i class="fas fa-list"></i> Listing <i class="fa-solid fa-caret-down"></i></a>
+                    <div class="Listings-Dropdown">
+                        <a href='../../Website/private/CRUD/createProduct.php'>List New Product</a>
+                        <a href="#">Edit Existing Product</a>
+                        <a href="#">Delete Existing Product</a>
+                    </div>
+                </li>
+            </ul>
 
-</html>
+            <!-- Searchbar - Take all product names and allow the user to search -->
+            <div class="search-bar">
+                <input type="text" placeholder="Search items or categories">
+                <button><i class="fas fa-search"></i></button>
+            </div>
+
+            <?php
+            if (isset($_SESSION['Active']) && $_SESSION['Active'] === true) {
+                echo '<li class="dropdown">
+                        <p class="username">Welcome, ' . htmlspecialchars($_SESSION['Username']) . '! <i class="fa-solid fa-caret-down"></i></p>
+                        <div class="Listings-Dropdown">
+                            <a href="Logout.php">Logout</a>
+                        </div>
+                    </li>';
+            } else {
+                echo '<div class="nav-buttons">
+                        <button class="join"><a href="signup.html">Get Started</a></button>
+                        <button class="login"><a href="login.php">Login</a></button>
+                    </div>';
+            }
+            ?>
+        </nav>
+    </body>
+
+    </html>
